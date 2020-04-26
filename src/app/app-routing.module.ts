0d727aa6aecb67042f9d0f321a7715bc55e52d41
Alan <alan_comsytem10@outlook.es>
2020-04-26 00:07:13 -0500

    Componentes agregados y enrutamietno

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { ContactanosComponent } from './pages/contactanos/contactanos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: PortafolioComponent },
+  { path: 'nosotros', component: AboutComponent },
+  { path: 'producto', component: ItemComponent },
+  { path: 'contactanos', component: ContactanosComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'inicio' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
