041b4e1660c27c449422847e75c0ed46167fa319
Alan <alan_comsytem10@outlook.es>
2020-04-27 21:26:58 -0500

    Agregado la priemr intrface, pagina con datos JSON

---
+++ src/app/interfaces/pagina-info.interface.ts
@@ -0,0 +1,9 @@
+
+export interface PaginaInfo{
+    titulo?: string;
+    email?: string;
+    nombre_corto?: string;
+    pagina_autor?: string;
+    facebook?: string;
+    instagram?: string;
+  }
--- src/app/services/pagina-info.service.ts
+++ src/app/services/pagina-info.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { PaginaInfo } from '../interfaces/pagina-info.interface';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaginaInfoService {
 
-  info: any = {};
+  info: PaginaInfo = {};
   cargada = false;
 
   constructor(private http: HttpClient) { 
 
     //Leer archivo JSON
     this.http.get('assets/data/data-betterware.json')
-        .subscribe(resp =>{
+        .subscribe((resp: PaginaInfo) => {
           this.cargada = true;
           this.info = resp;
           console.log(resp);
