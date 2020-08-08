a931dfbfdd9414db69c7e6e3a312861a09f81277
Alan_Cruz <alan_comsytem10@outlook.es>
2020-08-07 19:52:09 -0500

    Cambio de url en data

--- src/app/services/pagina-info.service.ts
+++ src/app/services/pagina-info.service.ts
@@ -11,15 +11,33 @@ export class PaginaInfoService {
   info: PaginaInfo = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor(private http: HttpClient) { 
 
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     //Leer archivo JSON
-    this.http.get('assets/data/data-betterware.json')
-        .subscribe((resp: PaginaInfo) => {
-          this.cargada = true;
-          this.info = resp;
-          console.log(resp);
-          
-        })
+    this.http.get('assets/data/data-laCroix.json')
+  
+    .subscribe((resp: PaginaInfo) => {
+      this.cargada = true;
+      this.info = resp;
+      
+      
+    });
+  }
+  private cargarEquipo(){
+    //Leer archivo JSON
+    this.http.get('https://perfumes-html.firebaseio.com/equipo.json')
+  
+    .subscribe((resp: any) => {
+      this.equipo = resp;
+      console.log(resp);
+    });
+
   }
 }
